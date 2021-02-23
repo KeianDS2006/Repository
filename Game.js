05a1267834f7ae58c4db8d5deeb0ccef785e0751
KeianDS2006 <74181700+KeianDS2006@users.noreply.github.com>
2021-02-23 18:18:40 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,55 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountref=await database.ref("player count").once("value");
+      if(playerCountref.exists()){
+        playerCount=playerCountref.val();
+        player.getCount();
+      }
+      
+      form = new Form()
+      form.display();
+    }
+  }
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120, 100);
+    player.getPlayerinfo();
+    if(allPlayers!==undefined){
+      var display_position=130;
+      if(plr=="player"+player.index){
+        fill("red");
+      }
+      else{
+        fill("black")
+      }
+      display_position+=20;
+      textSize(20);
+      text(allPlayers[plr].name+": "+allPlayers[plr].distance, 120, display_position);
+    }
+
+    if(keyIsDown("up")&&player.index!==null){
+      player.distance+=50;
+      player.update();
+    }
+  }
+  
+}
